8503b96ac1e46e1e5a742ec3fcf846c6fac63988
zhuyuan <zhuyuan@unitechs.com>
2020-12-17 16:07:13 +0800

    Remove Filter configuration from context

--- mouensis-web/src/main/java/com/mouensis/framework/web/configuration/WebFilterConfiguration.java
+++ mouensis-web/src/main/java/com/mouensis/framework/web/configuration/WebFilterConfiguration.java
@@ -1,6 +1,6 @@
-package com.mouensis.framework.context.configuration;
+package com.mouensis.framework.web.configuration;
 
-import com.mouensis.framework.context.filter.ContentCachingFilter;
+import com.mouensis.framework.web.filter.ContentCachingFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +9,10 @@ import org.springframework.context.annotation.Configuration;
  * 过滤器配置
  *
  * @author zhuyuan
- * @date 2020/12/1 16:31
+ * @date 2020/12/1 23:31
  */
 @Configuration
-public class FilterConfiguration {
+public class WebFilterConfiguration {
     @Bean
     public ContentCachingFilter contentCachingFilter() {
         return new ContentCachingFilter();
