f05b5e3a3d24b7c9881e19e299a07a104d57e3d8
zhuyuan <zhuyuan@unitechs.com>
2020-12-18 13:40:52 +0800

    Add Pagination static function

--- mouensis-web/src/main/java/com/mouensis/framework/web/domain/Pagination.java
+++ mouensis-web/src/main/java/com/mouensis/framework/web/domain/Pagination.java
@@ -1,9 +1,13 @@
 package com.mouensis.framework.web.domain;
 
+import com.mouensis.framework.web.resovler.PaginationSortResolver;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.util.Assert;
 
 import java.io.Serializable;
 import java.util.List;
@@ -44,5 +48,31 @@ public class Pagination implements Serializable {
     public Pagination(Integer page, Integer size, List<String> sort) {
         this(page, size);
         this.sort = sort;
+    }
+
+    /**
+     * 转换成JPA 分页信息
+     *
+     * @param entityClass
+     * @param <T>
+     * @return JPA分页对象
+     */
+    public <T> Pageable toPageable(Class<T> entityClass) {
+        return PageRequest.of(this.getPage(),
+                this.getSize(),
+                PaginationSortResolver.resolve(this.getSort(), entityClass));
+    }
+
+    /**
+     * JPA 分页信息转换成当前分页对象
+     *
+     * @param pageable JPA分页对象
+     * @return 当前类的实例
+     */
+    public static Pagination of(Pageable pageable) {
+        Assert.notNull(pageable, "pageable must be not null");
+        Pagination pagination = new Pagination(pageable.getPageNumber(), pageable.getPageSize(),
+                PaginationSortResolver.of(pageable.getSort()));
+        return pagination;
     }
 }
